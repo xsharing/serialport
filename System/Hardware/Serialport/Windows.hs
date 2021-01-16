c0faf08e5af449c597bf65311272e4f312153ebc
dopamane <dwc1295@gmail.com>
2021-01-16 01:48:42 -0500

    simplify windows handle usage, minor style/syntax updates

--- System/Hardware/Serialport/Windows.hs
+++ System/Hardware/Serialport/Windows.hs
@@ -3,9 +3,6 @@
 module System.Hardware.Serialport.Windows where
 
 import GHC.IO.Handle
-import GHC.IO.Device 
-import GHC.IO.BufferedIO
-import GHC.IO.Buffer
 
 import System.Win32.Types
 import System.Win32.File
@@ -13,7 +10,7 @@ import qualified System.Win32.Comm as Comm
 
 import Foreign.Marshal.Alloc
 
-import Control.Monad
+import Control.Monad (unless)
 
 import Data.Typeable
 import Data.Bits
@@ -28,47 +25,12 @@ data SerialPort = SerialPort
   , portSettings :: SerialPortSettings
   } deriving (Show, Typeable)
 
-
-instance RawIO SerialPort where
-  read (SerialPort h _) ptr n = return . fromIntegral =<< win32_ReadFile h ptr (fromIntegral n) Nothing
-  readNonBlocking _ _ _ = error "readNonBlocking not implemented"
-  write (SerialPort h _) ptr n = void (win32_WriteFile h ptr (fromIntegral n) Nothing)
-  writeNonBlocking _ _ _ = error "writenonblocking not implemented"
-
-
-instance IODevice SerialPort where
-  ready _ _ _ = return True
-  close = closeSerial
-  isTerminal _ = return False
-  isSeekable _ = return False
-  seek _ _ _ = return ()
-  tell _ = return 0
-  getSize _ = return 0
-  setSize _ _ = return ()
-  setEcho _ _ = return ()
-  getEcho _ = return False
-  setRaw _ _ = return ()
-  devType _ = return Stream
-
-
-instance BufferedIO SerialPort where
-  newBuffer _ = newByteBuffer 100
-  fillReadBuffer = readBuf
-  fillReadBuffer0 = readBufNonBlocking
-  flushWriteBuffer = writeBuf
-  flushWriteBuffer0 = writeBufNonBlocking
-
-
 -- |Open and configure a serial port returning a standard Handle.
-hOpenSerial :: String
-           -> SerialPortSettings
-           -> IO Handle
+hOpenSerial :: String -> SerialPortSettings -> IO Handle
 hOpenSerial dev settings = do
-  ser <- openSerial dev settings
-  h <- mkDuplexHandle ser dev Nothing noNewlineTranslation
+  h <- hANDLEToHandle . handle =<< openSerial dev settings
   hSetBuffering h NoBuffering
   return h
-
 
 -- | Open and configure a serial port
 openSerial :: String      -- ^ Serial port, such as @COM5@ or @CNCA0@
@@ -77,7 +39,7 @@ openSerial :: String      -- ^ Serial port, such as @COM5@ or @CNCA0@
 openSerial dev settings = do
   h <- createFile ("\\\\.\\" ++ dev) access_mode share_mode security_attr create_mode file_attr template_file
   let serial_port = SerialPort h defaultSerialSettings
-  return =<< setSerialSettings serial_port settings
+  setSerialSettings serial_port settings
   where
     access_mode = gENERIC_READ .|. gENERIC_WRITE
     share_mode = fILE_SHARE_NONE
@@ -89,10 +51,9 @@ openSerial dev settings = do
 
 -- |Receive bytes, given the maximum number
 recv :: SerialPort -> Int -> IO B.ByteString
-recv (SerialPort h _) n =
-  allocaBytes n $ \p -> do
-    recv_cnt <- win32_ReadFile h p count overlapped
-    B.packCStringLen (p, fromIntegral recv_cnt)
+recv port n = allocaBytes n $ \p -> do
+  recv_cnt <- win32_ReadFile (handle port) p count overlapped
+  B.packCStringLen (p, fromIntegral recv_cnt)
   where
     count = fromIntegral n
     overlapped = Nothing
@@ -102,9 +63,8 @@ recv (SerialPort h _) n =
 send :: SerialPort
         -> B.ByteString
         -> IO Int          -- ^ Number of bytes actually sent
-send (SerialPort h _) msg =
-  BU.unsafeUseAsCString msg $ \p ->
-    fromIntegral `fmap` win32_WriteFile h p count overlapped
+send port msg = BU.unsafeUseAsCString msg $ \p ->
+  fromIntegral `fmap` win32_WriteFile (handle port) p count overlapped
   where
     count = fromIntegral $ B.length msg
     overlapped = Nothing
@@ -112,12 +72,10 @@ send (SerialPort h _) msg =
 
 -- |Flush buffers
 flush :: SerialPort -> IO ()
-flush s@(SerialPort h _) =
-  flushFileBuffers h
-  >> consumeIncomingChars
+flush port = flushFileBuffers (handle port) >> consumeIncomingChars
   where
     consumeIncomingChars = do
-      ch <- recv s 1
+      ch <- recv port 1
       unless (ch == B.empty) consumeIncomingChars
 
 
@@ -128,32 +86,32 @@ closeSerial = closeHandle . handle
 
 -- |Set the Data Terminal Ready level
 setDTR :: SerialPort -> Bool -> IO ()
-setDTR (SerialPort h _ ) True =  Comm.escapeCommFunction h Comm.setDTR
-setDTR (SerialPort h _ ) False =  Comm.escapeCommFunction h Comm.clrDTR
+setDTR port True  =  Comm.escapeCommFunction (handle port) Comm.setDTR
+setDTR port False =  Comm.escapeCommFunction (handle port) Comm.clrDTR
 
 
 -- |Set the Ready to send level
 setRTS :: SerialPort -> Bool -> IO ()
-setRTS (SerialPort h _ ) True = Comm.escapeCommFunction h Comm.setRTS
-setRTS (SerialPort h _ ) False = Comm.escapeCommFunction h Comm.clrRTS
+setRTS port True  = Comm.escapeCommFunction (handle port) Comm.setRTS
+setRTS port False = Comm.escapeCommFunction (handle port) Comm.clrRTS
 
 
 -- |Configure the serial port
 setSerialSettings :: SerialPort           -- ^ The currently opened serial port
                   -> SerialPortSettings   -- ^ The new settings
                   -> IO SerialPort        -- ^ New serial port
-setSerialSettings (SerialPort h _) new_settings = do
+setSerialSettings port new_settings = do
   let ct = Comm.COMMTIMEOUTS {
                     Comm.readIntervalTimeout = maxBound :: DWORD,
                     Comm.readTotalTimeoutMultiplier = maxBound :: DWORD,
                     Comm.readTotalTimeoutConstant = fromIntegral (timeout new_settings) * 100,
                     Comm.writeTotalTimeoutMultiplier = 0,
                     Comm.writeTotalTimeoutConstant = 0 }
-  Comm.setCommTimeouts h ct
+  Comm.setCommTimeouts (handle port) ct
 
-  Comm.setCommState h new_settings
+  Comm.setCommState (handle port) new_settings
 
-  return (SerialPort h new_settings)
+  return $ SerialPort (handle port) new_settings
 
 
 -- |Get configuration from serial port
